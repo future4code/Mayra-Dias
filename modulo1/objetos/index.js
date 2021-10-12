4057a0447012fca54529e8f394940b55a971de20
Mayra <mb509311@gmail.comgit push origin git-1git config --global user.email mb509311@gmail.com>
2021-10-11 21:35:49 -0300

    objetos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,67 @@
+//1 a) 
+//  const pessoa = {
+//      nome: "Mayra", 
+//      apelidos: ["May", "Galeguinha"]   
+// }
+// console.log(`Eu sou ${pessoa.nome}, mas pode me chamar de: ${pessoa.apelidos[0]} ou ${pessoa.apelidos[1]}.`)
+//b)
+// function novosApelidos(apelidosA) {
+//      pessoa.apelidos[0]= "Galega"
+//      pessoa.apelidos[1]= "Loirinha"
+// }
+//      console.log(novosApelidos(pessoa))
+     
+//2 a)
+const pessoa = {
+	nome: "Bruna", 
+    idade: 26, 
+	profissao: "Jornalista"
+}
+
+const pessoaB = {
+	nome: "Bruno", 
+    idade: 23, 
+	profissao: "Instrutor"
+}
+//b)
+function minhaFuncao(pessoa) {
+  const resultado=[]
+  nome.length
+  idade.length
+  profissao.length
+  return resultado
+}
+
+console.log(minhaFuncao(pessoa))
+
+
+//3 a)
+//      const carrinho= [];
+
+//b)
+//      const fruta1={
+//      nome:"Ameixa",
+//      disponibilidade: true
+    
+// };
+//      const fruta2= {
+//      nome:"Melão",
+//      disponibilidade: true
+
+// };
+//      const fruta3= {
+//      nome:"Banana",
+//      disponibilidade: true
+
+//c)
+// };
+//    function adicionarNoCarrinho(fruta) {
+//     carrinho.push(fruta)
+//d)    
+// } 
+// adicionarNoCarrinho(fruta1)
+// adicionarNoCarrinho(fruta2)
+// adicionarNoCarrinho(fruta3)
+// console.log(carrinho)
+
+ 
