3ada20857f4a1e5f708c7a13c8ff54adf5b5da25
Mayra <mb509311@gmail.comgit push origin git-1git config --global user.email mb509311@gmail.com>
2021-10-13 17:53:10 -0300

    condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,57 @@
+//1 - Exercícios de interpretação de código
+//a) Para saber se são iguais ou não
+//b) números iguais
+//c) números diferentes
+//2
+//a) Para que o usuario escolha uma fruta que esteja no codigo.
+//b)O preço da fruta, maçã é R$ 2.25
+//c) Ia aparecer o preço da pera e o que tem logo abaixo.
+//3
+//a) Pedindo que o usuario digite um numero
+//b) "Esse número passou no teste")
+//c)Dentro de um IF é um ELSE 
+
+
+// //1 - Exercícios de escrita de código
+// const idade= Number(prompt("Qual a sua idade?"))
+//  if(idade> 17 ){
+//    console.log('Você pode dirigir.');
+// }else if(idade< 18 ){
+//    console.log('Você não pode dirigir.');
+// }
+
+//2
+//  const turno=prompt("Qual o seu turno, digite M (matutino) ou V (Vespertino) ou N (Noturno)")
+
+//  if(turno=== "M"){
+//      console.log("Bom dia!");
+//  }else if(turno=== "V"){
+//      console.log("Boa tarde!")
+//  }else if(turno=== "N"){
+//     console.log("Boa noite!")
+//  }
+
+//3 
+// let turno=prompt("Qual o seu turno, digite M (matutino) ou V (Vespertino) ou N (Noturno)")
+// switch (turno){
+//      case 'M':
+//       console.log('Bom dia!') 
+//          break
+//      case 'V':
+//        console.log('Boa tarde!')
+//           break
+//      case 'N':
+//     console.log('Boa noite!')
+//           break
+//}
+
+//4
+// const generoDoFilme= prompt("Qual o gênero do filme?")
+// const precoDoFilme=Number(prompt("Valor do ingresso"))
+
+// if (generoDoFilme === 'fantasia' && precoDoFilme < 15){
+//      console.log("Bom Filme!")
+// }
+// else if (generoDoFilme === 'fantasia' || precoDoFilme > 15){
+//      console.log("Escolha outro filme!")
+// }
