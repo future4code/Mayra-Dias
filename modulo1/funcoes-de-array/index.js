88e209c99a6be241e645cd843cc576e6efae309a
Mayra <mb509311@gmail.comgit push origin git-1git config --global user.email mb509311@gmail.com>
2021-10-15 18:18:11 -0300

    funcoes-de-array

---
+++ modulo1/funcoes-de-array/index.js
@@ -0,0 +1,75 @@
+//Exercícios de interpretação de código
+//1 - a) nome: "Amanda Rangel", apelido: "Mandi" },
+ // { nome: "Laís Petra", apelido: "Laura" },
+ // { nome: "Letícia Chijo", apelido: "Chijo"}
+
+ //2 - {nome: "Amanda Rangel"
+ //{nome: "Laís Petra"
+//{nome: "Letícia Chijo"
+
+//3 Chijo"
+
+
+//Exercícios de escrita de código
+//1 - 
+// const pets = [
+//     { nome: "Lupin", raca: "Salsicha"},
+//     { nome: "Polly", raca: "Lhasa Apso"},
+//     { nome: "Madame", raca: "Poodle"},
+//     { nome: "Quentinho", raca: "Salsicha"},
+//     { nome: "Fluffy", raca: "Poodle"},
+//     { nome: "Caramelo", raca: "Vira-lata"},
+//]
+//a)
+// const pegarNomeDosPets = (pet) => {
+//     return {nome: pet.nome}
+// }
+// const nomesDosPets = pets.filter(pegarNomeDosPets).map(pegarNomeDosPets) 
+// console.log(nomesDosPets)
+
+//b)
+//  const pegarRacasDosPets = (cachorroSalsicha) => {
+//     return cachorroSalsicha.raca === "Salsicha"
+
+//  }
+// const pegarRacaDosPets= (cachorroSalsicha) =>{
+//     return cachorroSalsicha.nome
+// }
+
+//  const racasDosPets = pets.filter(pegarRacasDosPets).map(pegarRacaDosPets) 
+//  console.log(racasDosPets)
+
+//c)
+// const poodleCachorro = (cachorro) => {
+//     return cachorro.raca === "Poodle"
+
+// } 
+// const racaPoodle = (cachorro) => {
+//     return cachorro.nome
+
+// }
+
+// const poodleRaca = pets.filter(poodleCachorro).map(racaPoodle) 
+// console.log(poodleRaca)
+// console.log(`Você ganhou um cupom de desconto de 10% para tosar o/a ${poodleRaca[0]}!`)
+// console.log(`Você ganhou um cupom de desconto de 10% para tosar o/a ${poodleRaca[1]}!`)
+
+//2
+// const produtos = [
+//     { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+//     { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+//     { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+//     { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+//     { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+//     { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+//     { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+//     { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+//     { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+//     { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+//a)
+//  const nomeDosProdutos = (produto) => {
+//    return produto.nome
+//  }
+//  const produtosNomes = produtos.filter(nomeDosProdutos).map(nomeDosProdutos) 
+//  console.log(produtosNomes)
+
