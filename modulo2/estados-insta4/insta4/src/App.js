c2d6178a0435631e4b9ecb59c16609abb1369c66
Mayra <mb509311@gmail.comgit push origin git-1git config --global user.email mb509311@gmail.com>
2021-11-05 19:49:26 -0300

    insta4

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -12,19 +12,29 @@ const MainContainer = styled.div`
 class App extends React.Component {
   render() {
     return (
+      <div>
       <MainContainer>
         <Post
           nomeUsuario={'paulinha'}
           fotoUsuario={'https://picsum.photos/50/50'}
           fotoPost={'https://picsum.photos/200/150'}
         />
+         <Post
+          nomeUsuario={'Ana'}
+          fotoUsuario={'https://picsum.photos/id/1011/367/267'}
+          fotoPost={'https://picsum.photos/id/367/367/267'}
+        />
+         <Post
+          nomeUsuario={'Catarina'}
+          fotoUsuario={'https://picsum.photos/id/399/367/267'}
+          fotoPost={'https://picsum.photos/id/274/367/267'}
+        />
       </MainContainer>
+    </div>
     );
   }
 }
 
-export default App;
-
-
+ export default App;
 
 
