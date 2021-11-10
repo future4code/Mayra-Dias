7a4b8739f0c5b781ce47997187b052a5504348f0
Mayra <mb509311@gmail.comgit push origin git-1git config --global user.email mb509311@gmail.com>
2021-11-09 22:38:29 -0300

    ciclo-vida

---
+++ modulo2/rend-condicional-form/condicional-form/src/Etapa2.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+
+
+function Etapa2() {
+    return (
+      <div className="Etapa1">
+        <header className="Etapa1-header">
+          <ol>
+            <li>Qual o seu nome?</li>
+            <li>Qual sua idade?</li>
+            <li>Qual seu email?</li>
+            <li>Qual a sua escolaridade?</li>
+          </ol>
+           
+          
+         
+        </header>
+      </div>
+    );
+  }
+  
+  export default Etapa2;
