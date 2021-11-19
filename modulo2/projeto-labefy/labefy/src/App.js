f4ed15b97632432fb48afcc06db4d54fdc531fdc
Mayra <mb509311@gmail.comgit push origin git-1git config --global user.email mb509311@gmail.com>
2021-11-19 19:33:27 -0300

    labefy

--- modulo2/projeto-labefy/labefy/src/App.js
+++ modulo2/projeto-labefy/labefy/src/App.js
@@ -3,19 +3,41 @@ import AdicionarMusicas from './components/AdicionarMusica';
 import Playlist from './components/Playlist';
 
 
-export default class App extends React.Component() {
+
+export default class App extends React.Component{
  state = {
 
-telaAtual: "Playlist de músicas"
+telaAtual: "Adicionar Musicas"
 
  }
- 
+ escolheTela = () =>{
+  switch(this.state.telaAtual){
+    case "Adicionar Musicas":
+     return <AdicionarMusicas irParaPlaylist={this.irParaPlaylist}/>
+    case "Playlist":
+      return <Playlist irParadicionarMusicas={this.irParadicionarMusicas}/>
+    default:
+      return <div>Erro! Página não encontrada</div>
+  }
+
+}
+
+irParadicionarMusicas = () =>{
+ this.setState({telaAtual: "Adicionar Musicas"})
+
+}
+
+irParaPlaylist = () =>{
+  this.setState({telaAtual: "Playlist"})
+
+
+}
  
  render(){
   return (
     <div>
-      <AdicionarMusicas/>
-      <Playlist/>
+      {<h2>Labefy</h2>}
+      {this.escolheTela()}
     </div>
   );
 }
--- modulo2/projeto-labefy/labefy/src/components/Playlist.js
+++ modulo2/projeto-labefy/labefy/src/components/Playlist.js
@@ -1,9 +1,60 @@
 import React from 'react'
+import axios from "axios"
+import styled from 'styled-components'
+
+const Card = styled.div`
+border: 1px solid black;
+padding: 10px;
+margin: 10px
+
+`
 
 export default class Playlist extends React.Component{
+
+state = {
+ musicas: []
+
+}
+
+componentDidMount(){
+this.pegarMusicas()
+
+}
+
+
+   pegarMusicas = () =>{
+
+      const url = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists"
+      axios.get(url,{
+        headers: {
+         Authorization: "Mayra-Barbosa-carver"
+      
+        }
+      }).then((res) => {
+      this.setState({musicas: res.data})
+      
+      }).catch((err) => {
+      alert("Ocorreu um problema, tente novamente.")
+      
+      })
+      
+      }
+      
+
  render(){
+
+const listaMusicas = this.state.musicas.map((user) =>{
+return <Card>{user.name}</Card>
+
+
+})
+
    return(
-      <div>Playlist</div>
+      <div>
+      <button onClick={this.props.irParadicionarMusicas}>Voltar página</button>
+      <h2>Playlist</h2>
+      {listaMusicas}
+      </div>
 
    )
 
