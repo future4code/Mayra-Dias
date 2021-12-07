97863aed8071db60dad7268a35cda81f21869e53
Mayra <mb509311@gmail.com>
2021-12-07 20:43:15 -0200

    criando a pasta router

--- modulo3/projeto-labex/labex/src/App.js
+++ modulo3/projeto-labex/labex/src/App.js
@@ -1,48 +1,13 @@
 import React from 'react'
-import {AdminHomePage} from './pages/AdminHomePage';
-import {ApplicationFormPage} from './pages/ApplicationFormPage';
-import {CreateTripPage} from './pages/CreateTripPage';
-import {HomePage} from './pages/HomePage';
-import {ListTripsPage} from './pages/ListTripsPage';
-import {LoginPage} from './pages/LoginPage';
-import {TripDetailsPage} from './pages/TripDetailsPage';
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {Router} from './router/Router';
+
 
 export default function App() {
   return (
-   <BrowserRouter>
-      <Switch>
-       
-      <Route exact path={"/"}>
-      <HomePage/>
-      </Route>
-
-      <Route exact path={"/trips/list"}>
-      <ListTripsPage/>
-      </Route>
-
-      <Route exact path={"/trips/application"}>
-      <ApplicationFormPage/>
-      </Route>
-      
-      <Route exact path={"/login"}>
-      <LoginPage/>
-      </Route>
-      
-      <Route exact path={"/admin/trips/list"}>
-      <AdminHomePage/>
-      </Route>
-
-      <Route exact path={"/admin/trips/create"}>
-      <CreateTripPage/>
-      </Route>
-
-      <Route exact path={"/admin/trips/:id"}>
-      <TripDetailsPage/>
-      </Route>
-      
-    </Switch>
-  </BrowserRouter>
+   <div>
+     <Router/>
+   </div>
+   
   );
 }
 
