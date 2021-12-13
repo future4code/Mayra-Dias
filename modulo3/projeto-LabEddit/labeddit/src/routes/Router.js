08c581c2ffbd26c1beee5175f6b4d53a63d75189
Mayra <mb509311@gmail.com>
2021-12-13 19:44:14 -0200

    criando as rotas

---
+++ modulo3/projeto-LabEddit/labeddit/src/routes/Router.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import CadastroPage from '../pages/CadastroPage/CadastroPage'
+import ErrorPage from '../pages/ErrorPage/ErrorPage'
+import FeedPage from '../pages/FeedPage/FeedPage'
+import LoginPage from '../pages/LoginPage/LoginPage'
+import PostPage from '../pages/PostPage/PostPage'
+
+const Router = () => {
+return(
+    
+<BrowserRouter>
+  <Switch>
+       <Route exact path="/login">
+            <LoginPage/>
+        </Route>
+        <Route exact path="/cadastro">
+            <CadastroPage/>
+        </Route>
+        <Route exact path="/pagina-feed/:id">
+            <FeedPage/>
+        </Route>
+        <Route exact path="/pagina-post">
+            <PostPage/>
+        </Route>
+        <Route>
+            <ErrorPage/>
+        </Route>
+    </Switch>
+ </BrowserRouter>
+
+)}
+
+export default Router;
---
+++ modulo3/projeto-LabEddit/labeddit/src/routes/coordinator.js
@@ -0,0 +1,12 @@
+export const goToLogin = (history) =>{
+    history.push("/login")
+}
+export const goToCadastro = (history) =>{
+    history.push("/cadastro")
+}
+export const goToFeed = (history) =>{
+    history.push("/pagina-feed/:id")
+}
+export const goToPost = (history) =>{
+    history.push("/pagina-post")
+}
