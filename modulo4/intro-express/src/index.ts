44325d03e5aca510b6581b3ffac0670c803148ea
Mayra <mb509311@gmail.com>
2022-01-24 20:19:05 -0200

    intro-express

---
+++ modulo4/intro-express/src/index.ts
@@ -0,0 +1,27 @@
+// // import express from "express";
+// import { AddressInfo } from "net";
+
+// // const app = express();
+
+// app.use(express.json());
+
+// const server = app.listen(process.env.PORT || 3003, () => {
+//   if (server) {
+//     const address = server.address() as AddressInfo;
+//     console.log(`Server is running in http://localhost:${address.port}`);
+//   } else {
+//     console.error(`Failure upon starting server.`);
+//   }
+// });;
+
+// app.get("/", (req, res) =>{
+// res.send("Olá")
+// })
+
+// type User ={
+// id:number,
+// name:string,
+// phone:string,
+// email:string,
+// website:string,
+// }
