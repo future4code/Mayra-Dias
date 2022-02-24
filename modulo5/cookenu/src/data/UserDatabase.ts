17c85e7f129eadea28b3fcb599206c8e383e97a6
Mayra <mb509311@gmail.com>
2022-02-24 19:23:05 -0300

    entrega

--- modulo5/cookenu/src/data/UserDatabase.ts
+++ modulo5/cookenu/src/data/UserDatabase.ts
@@ -1,19 +1,32 @@
-// import { User } from "../entities/User";
-// import { BaseDatabase } from "./BaseDatabase";
+import { User } from "../entities/User";
+import { BaseDatabase } from "./BaseDatabase";
 
-// export class UserDatabase extends BaseDatabase{
-//     public async findUser(email:string): User{
-//         try{
-//             const user = await BaseDatabase.connection("user")
-//             .select('*')
-//             .where({email}); const user:any[]
+export class UserDatabase extends BaseDatabase{
+ public async createUser(user:User): Promise<void>{
+     try{
+       await BaseDatabase.connection('user').insert({
+           id: user.getId(),
+           name: user.getName(),
+           email: user.getEmail(),
+           password: user.getPassword()
+       })
+     }catch(error){
+       throw new Error(error.sqlMessage || error.message)
+     }
+ }
 
-//         return User.toUseMode(user[0]);
 
-//         }cath(error){
-//             throw new Error(error.sqlMessage || error.message)
+    public async findUser(email:string):Promise<User>{
+        try{
+            const user = await BaseDatabase.connection("user")
+            .select('*')
+            .where({email});
 
-//         }
-//     } 
+        return User.toUseMode(user[0]);
+            }catch(error){
+            throw new Error(error.sqlMessage || error.message)
 
-// }
+        }
+    } 
+
+}
--- modulo5/cookenu/src/endpoints/signup.ts
+++ modulo5/cookenu/src/endpoints/signup.ts
@@ -1,4 +1,8 @@
 import { Request, Response } from "express";
+import { UserDatabase } from "../data/UserDatabase";
+import { User } from "../entities/User";
+import { Authenticator } from "../services/Authenticator";
+import { HashManager } from "../services/HashManager";
 import { Id } from "../services/Id";
 
 export async function signup(req: Request, res:Response){
@@ -8,12 +12,25 @@ export async function signup(req: Request, res:Response){
          res.status(422).send("Insira corretamente")
      }
      
+      const userDatabase = new UserDatabase()
+      const user = userDatabase.findUser(email)
 
+      if(user){
+          res.status(409).send("Esse enail já está cadastrado!")
+      }
       const id = new Id();
       const idGenerator = id.generate()
 
+      const hashManager = new HashManager()
+      const hashPassword = await hashManager.hash(password);
 
+      const newUser = new User(idGenerator, name, email, hashPassword)
+      await userDatabase.createUser(newUser)
 
+      const authenticator = new Authenticator()
+      
+
+      res.status(200).send("Usuário criado!")
     }catch(error){
         res.status(400).send(error.message)
     }
